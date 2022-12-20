74cdf64537ecbbdd8d2fa411ed0a7c4f4f7ab97b
rajashree112 <107317983+rajashree112@users.noreply.github.com>
2022-12-20 20:09:15 +0530

    Add files via upload

---
+++ Reverse pairs/main.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    double  width= 3.0;
+    double height= 2.3;
+    double perimeter = 2.0*(height+width);
+    double area = width*height;
+    printf("%f\n",perimeter);
+    printf("\n%f",area);
+}
